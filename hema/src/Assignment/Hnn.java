aecd932ad09ebb42aab71dd675d030772aa11540
Hemalatha <Hemalatha@DESKTOP-E4NIMS8>
2022-05-12 12:50:25 +0530

    Username

---
+++ hema/src/Assignment/Hnn.java
@@ -0,0 +1,16 @@
+package Assignment;
+
+public class Hnn 
+{
+	 public static void main(String[] args)
+	 {
+		 String s1 = "rathnnn";
+		 String s2 = "";
+		 for(int i=s1.length()-1; i>=0; i--)
+		 {
+			 System.out.println(s2);
+		 }
+		 
+	 }
+
+}
